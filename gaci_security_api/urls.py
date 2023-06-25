260ee7b0f2f918fb226362610ac4e5bd1709191a
victoiremupikano <ushindimupikano@gmail.com>
2023-06-26 01:28:49 +0300

    ajout des urls seconddaires dans urls.py de l'app principale

--- gaci_security_api/urls.py
+++ gaci_security_api/urls.py
@@ -1,7 +1,7 @@
-"""gaci_security_api URL Configuration
+"""cadastre URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# on fait quelque importation pour la manip des medias
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/auth/", include("authentication.urls")),
 ]
+
+#on charger l'url vers les medias
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
