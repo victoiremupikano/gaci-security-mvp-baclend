d4caa8a19fe2cb823bc1d4e0fc4841e702f73b32
victoiremupikano <ushindimupikano@gmail.com>
2023-06-26 02:24:25 +0300

    ajout des models dans admin.py

--- security_data/admin.py
+++ security_data/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . models import Want_To_Research, Curfew_And_Instability, Population_Alert
 
 # Register your models here.
+admin.site.register(Want_To_Research)
+admin.site.register(Curfew_And_Instability)
+admin.site.register(Population_Alert)
