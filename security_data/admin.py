2bd2c2e049b3a8c6be846d6bdabfaa2ee163dc79
victoiremupikano <ushindimupikano@gmail.com>
2023-06-26 02:24:54 +0300

    ajout des models dans admin.py de security-data comme app

--- security_data/admin.py
+++ security_data/admin.py
@@ -3,5 +3,5 @@ from . models import Want_To_Research, Curfew_And_Instability, Population_Alert
 
 # Register your models here.
 admin.site.register(Want_To_Research)
-admin.site.register(Curfew_And_Instability)
 admin.site.register(Population_Alert)
+admin.site.register(Curfew_And_Instability)
